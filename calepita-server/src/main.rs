46661f63719b4e34143f37e6442fdcc6c262f8e3
sudonotfound <koxxxm@icloud.com>
2025-05-12 14:28:12 +0900

    add main.rs

--- calepita-server/src/main.rs
+++ calepita-server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use dotenvy::dotenv;
+use std::net::SocketAddr;
+use tracing_subscriber;
+
+mod routes;
+mod firestore;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(routes::health_check))
+        .route("/schedules", get(routes::get_schedules)); // 仮置き
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
