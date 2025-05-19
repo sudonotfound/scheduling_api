2101affa05678975551d4eb38cca8f8801659913
sudonotfound <koxxxm@icloud.com>
2025-05-19 10:13:54 +0900

    main.rsを修正

--- calepita-server/src/main.rs
+++ calepita-server/src/main.rs
@@ -1,24 +1,25 @@
 use axum::{routing::get, Router};
 use dotenvy::dotenv;
+use std::env;
 use std::net::SocketAddr;
 use tracing_subscriber;
 
-mod routes;
-mod firestore;
-
 #[tokio::main]
 async fn main() {
+    // .env ファイルの読み込み（任意）
     dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(routes::health_check))
-        .route("/schedules", get(routes::get_schedules)); // 仮置き
+    let app = Router::new().route("/", get(root));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("Listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
         .await
         .unwrap();
+}
+
+async fn root() -> &'static str {
+    "カレピタへようこそ！"
 }
